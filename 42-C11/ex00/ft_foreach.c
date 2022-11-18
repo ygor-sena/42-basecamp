7ad8ccd4bb0d1a3286b4700ac67a2a597cae1f65
Ygor Sena <yde-goes@student.42sp.org.br>
2022-11-18 18:03:49 -0300

    chore: remove executable file and add main test

--- 42-C11/ex00/ft_foreach.c
+++ 42-C11/ex00/ft_foreach.c
@@ -6,16 +6,35 @@
 /*   By: ygorgsena <ygorgsena@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:33:28 by ygorgsena         #+#    #+#             */
-/*   Updated: 2022/11/16 18:50:09 by ygorgsena        ###   ########.fr       */
+/*   Updated: 2022/11/16 19:19:04 by ygorgsena        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_intlen(int *s);
-
-void	ft_foreach(int *tab, int length, void(*f)(int))
+void	ft_foreach(int *tab, int length, void (*f)(int))
 {
-	while (--length >= 0)
+	int	i;
+
+	i = 0;
+	while (i < length)
+	{
 		f(tab[length]);
+		i++;
+	}	
 }
+
+/* void plus_two(int nbr)
+{
+	nbr += 2;
+	printf("%d\n", nbr);
+}
+
+int	main(void)
+{
+	int tab[] = { 5, 6, 7, 10} ;
+	
+	ft_foreach(tab, 5, plus_two);
+	for (int i = 0; i < 4; i++)
+		printf("tab %d is %d\n", i, tab[i]);
+} */
