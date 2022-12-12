3948f7a1736dfd92081b38efe765809076c8d785
Ygor Sena <yde-goes@student.42sp.org.br>
2022-12-12 17:34:25 -0300

    feat: create C12 folder and add ex00

---
+++ 42-C12/ex00/ft_list.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ygorgsena <ygorgsena@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/11 20:41:40 by ygorgsena         #+#    #+#             */
+/*   Updated: 2022/12/11 21:12:19 by ygorgsena        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}					t_list;
+
+t_list	*ft_create_elem(void *data);
+
+#endif
