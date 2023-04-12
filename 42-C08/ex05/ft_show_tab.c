9b55ccc7a6208a3a3f56fa7670741bf2399765e1
ygor_sena <yde-goes@student.42sp.org.br>
2023-04-11 23:01:54 -0300

    feat: add solution of ex05

---
+++ 42-C08/ex05/ft_show_tab.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/06 23:14:02 by yde-goes          #+#    #+#             */
+/*   Updated: 2023/04/06 23:23:40 by yde-goes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	index;
+
+	index = 0;
+	while (par[index] != NULL)
+	{
+		ft_putstr(par[index].str);
+		ft_putchar('\n');
+		ft_putnbr(par[index].size);
+		ft_putchar('\n');
+		ft_putstr(par[index].copy);
+		ft_putchar('\n');
+		index++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb <= 9)
+	{
+		ft_putchar(nb + '0');
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+}
---
+++ 42-C08/ex05/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yde-goes <yde-goes@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 20:07:37 by yde-goes          #+#    #+#             */
+/*   Updated: 2023/04/06 23:14:58 by yde-goes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+#endif
